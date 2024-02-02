65222b36fd68eec29de9fdcc06011eca5ddf8ae7
Diyorbek-Majidov-2110125 <2110125@newuu.uz>
2024-02-02 12:47:27 +0500

    updated

--- lib/Screens/login/splash_screen.dart
+++ lib/Screens/login/splash_screen.dart
@@ -30,7 +30,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
+      color: Colors.blue,
       child: Stack(
         children: [
           CustomPaint(
